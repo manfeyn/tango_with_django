096c85e745eccc861585f753d10c5f3abdb70ff8
manfeyn <manfeyn@gmail.com>
2015-09-07 21:18:06 +0800

    chp 4

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+	return HttpResponse("Rango says hey there world!<br/><a href='/rango/about/'>About</a>")
+
+def about(request):
+	return HttpResponse("About page. <br/><a href='/rango/'>Index</a>") 
